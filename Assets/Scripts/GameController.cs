55b95ea9981e4aa1f4ea2502b6fd57c5675bdfee
Luke Harper <lukeharper@live.ca>
2017-03-05 12:36:29 -0500

    added a game controller
    game controller for the camera and game state

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Update called once per frame after every update
+    void LateUpdate()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float zAxisValue = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+        Debug.Log("Camera Coords: " +transform.position.ToString());
+    }
+}
