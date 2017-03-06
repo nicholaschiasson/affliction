ecea17dd20d456905189a6408aff2439f735d6ff
Nicholas Omer Chiasson <nicholaschiasson@namnamnamnam.com>
2017-03-05 19:16:37 -0500

    Adding some colliders to microorganisms

--- Assets/Scripts/RedBloodCell.cs
+++ Assets/Scripts/RedBloodCell.cs
@@ -1,12 +1,15 @@
-﻿public class RedBloodCell : BloodCell {
+﻿public class RedBloodCell : BloodCell
+{
 
 	// Use this for initialization
-	protected override void Start () {
-        base.Start();
+	protected override void Start()
+	{
+		base.Start();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
 }
