1dec017ef89f8fd83f49043edf4c7c4baa3de120
Nicholas Omer Chiasson <nicholaschiasson@namnamnamnam.com>
2017-03-05 21:08:54 -0500

    Fixing infection script to inherit from monobehavior

--- Assets/Scripts/Infection.cs
+++ Assets/Scripts/Infection.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Infection {
+public class Infection : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
 
-    // Use this for initialization
-    void Start () {
-		
-    }
-	
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
 }
