691f2a5f36f8343197d43292dafe890524b487c4
Nicholas Omer Chiasson <nicholaschiasson@namnamnamnam.com>
2017-03-05 22:13:51 -0500

    Implemented drawing selection box

--- Assets/Scripts/View/SelectionBox.cs
+++ Assets/Scripts/View/SelectionBox.cs
@@ -2,4 +2,41 @@
 
 public class SelectionBox : MonoBehaviour
 {
+	Vector2 p1 = Vector2.zero;
+	Vector2 p2 = Vector2.zero;
+
+	void Update()
+	{
+		Vector3 mousePos = Input.mousePosition;
+		if (Input.GetMouseButtonDown(0))
+		{
+			p1 = new Vector2(mousePos.x, Screen.height - mousePos.y);
+		}
+		if (Input.GetMouseButton(0))
+		{
+			p2 = new Vector2(mousePos.x, Screen.height - mousePos.y);
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			p1 = Vector2.zero;
+			p2 = Vector2.zero;
+		}
+	}
+
+	void OnGUI()
+	{
+		if (p1 != Vector2.zero || p2 != Vector2.zero)
+		{
+			Rect drawRect = new Rect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+			DrawQuad(drawRect, new Color(1.0f, 1.0f, 1.0f, 0.5f));
+		}
+	}
+
+	void DrawQuad(Rect position, Color color)
+	{
+		Texture2D texture = new Texture2D(1, 1);
+		texture.SetPixel(0, 0, color);
+		texture.Apply();
+		GUI.DrawTexture(position, texture);
+	}
 }
