b7680cd553f69476bf6e9354d38daf60627acd13
Nicholas Omer Chiasson <nicholaschiasson@namnamnamnam.com>
2017-03-06 22:09:56 -0500

    Update selection box to properly adjust size on camera movement

--- Assets/Scripts/View/SelectionBox.cs
+++ Assets/Scripts/View/SelectionBox.cs
@@ -2,42 +2,45 @@
 
 public class SelectionBox : MonoBehaviour
 {
-	Transform mainCamera = null;
-	Vector2 p1 = Vector2.zero;
+	Camera mainCamera = null;
+	Vector3 p1 = Vector3.zero;
 	Vector2 p2 = Vector2.zero;
-	Vector2 c = Vector2.zero;
 
 	void Awake()
 	{
-		mainCamera = Camera.main.transform;
+		mainCamera = Camera.main;
 	}
 
-	// TODO: Have selection box size appropriately update with camera movement
 	void Update()
 	{
 		Vector3 mousePos = Input.mousePosition;
 		if (Input.GetMouseButtonDown(0))
 		{
-			p1 = new Vector2(mousePos.x, Screen.height - mousePos.y);
-			c = new Vector2(mainCamera.position.x, mainCamera.position.z);
+			Plane plane = new Plane(Vector3.up, Vector3.zero);
+			Ray ray = Camera.main.ScreenPointToRay(mousePos);
+			float distance;
+			if (plane.Raycast(ray, out distance))
+			{
+				p1 = ray.GetPoint(distance);
+			}
 		}
 		if (Input.GetMouseButton(0))
 		{
-			p2 = new Vector2(mousePos.x, Screen.height - mousePos.y);
+			p2 = new Vector2(mousePos.x, mousePos.y);
 		}
 		if (Input.GetMouseButtonUp(0))
 		{
-			p1 = Vector2.zero;
+			p1 = Vector3.zero;
 			p2 = Vector2.zero;
-			c = Vector2.zero;
 		}
 	}
 
 	void OnGUI()
 	{
-		if (p1 != Vector2.zero || p2 != Vector2.zero)
+		if (p1 != Vector3.zero || p2 != Vector2.zero)
 		{
-			Rect drawRect = new Rect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+			Vector3 p = mainCamera.WorldToScreenPoint(p1);
+			Rect drawRect = new Rect(p.x, Screen.height - p.y, p2.x - p.x, -(p2.y - p.y));
 			DrawQuad(drawRect, new Color(1.0f, 1.0f, 1.0f, 0.5f));
 		}
 	}
