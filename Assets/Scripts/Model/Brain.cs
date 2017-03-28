ce68505938a10631979a0de8d40c1673cc4e963c
Luke Harper <lukeharper@live.ca>
2017-03-28 16:42:03 -0400

    Created a Brain

---
+++ Assets/Scripts/Model/Brain.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Brain : Organ
+{
+    public Organ[] organs;
+    int baseCost = 1000;
+    protected override void Awake()
+    {
+        base.Awake();
+        
+    }
+}
